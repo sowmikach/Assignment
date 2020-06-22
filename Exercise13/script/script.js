458b1c630b36549272b7436b8adb623736830b1b
Shruthi Bhatt <Shruthi.SatheendranathBhatt@siriuscom.com>
2020-06-22 16:18:26 +0530

    Ex 13 - Review Comments added

--- Exercise13/script/script.js
+++ Exercise13/script/script.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
-var biryani_items = ["CHICKEN BIRYANI", "MUTTON BIRYANI", "VEG BIRYANI", "PANEER BIRYANI"];
+(function(){
+  var biryani_items = ["CHICKEN BIRYANI", "MUTTON BIRYANI", "VEG BIRYANI", "PANEER BIRYANI"];
 var pizza_items = ["CHICKEN PIZZA", "VEG PIZZA", "CHEESE PIZZA"];
 
 function displayMainMenu(){
@@ -13,6 +14,7 @@ function displayMainMenu(){
 function displayBiryaniMenu(){
   document.getElementById("food").style.display="none";
   document.getElementById("biryani").style.display="block";
+  //Todo: Innclude a let/const/var for all variables
   str="";
   for(var i=0; i<biryani_items.length; i++){
     str+="<li id="+i+" onClick='addToCart_b(this.id)'>"+biryani_items[i]+"</li>";
@@ -26,20 +28,22 @@ function displayPizzaMenu(){
   document.getElementById("pizza").style.display="block";
   str="";
   for(var i=0; i<pizza_items.length; i++){
-    str+="<li id="+i+" onClick='addToCart_p(this.id)'>"+pizza_items[i]+"</li>";
+    str+="<li id="+i+" onClick='this.addToCart_p(this.id)'>"+pizza_items[i]+"</li>";
   }
   document.getElementById("pizzaVarieties").innerHTML=str;
   document.getElementById("back_p").onclick= displayMainMenu;
 }
+  //Todo: Innclude all global variables at start of file
 
 var cart="";
 function addToCart_b(id_value){
     cart+="<li>"+biryani_items[id_value]+"</li>";
     document.getElementById("addToCart").innerHTML=cart;
 }
-
+// TODO:Function logic is repeated. Make it reusable
 function addToCart_p(id_value){
     cart+="<li>"+pizza_items[id_value]+"</li>";
     document.getElementById("addToCart").innerHTML=cart;
 }
 displayMainMenu();
+})();
