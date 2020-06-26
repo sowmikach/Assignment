f596933ec64ec92c90b76594b0ffd6ed4e50275d
sowmikach <65588608+sowmikach@users.noreply.github.com>
2020-06-26 20:17:12 +0530

    Update script.js

--- Exercise13/script/script.js
+++ Exercise13/script/script.js
@@ -1,49 +1,68 @@
 //jshint esversion:6
-(function(){
-  var biryani_items = ["CHICKEN BIRYANI", "MUTTON BIRYANI", "VEG BIRYANI", "PANEER BIRYANI"];
-var pizza_items = ["CHICKEN PIZZA", "VEG PIZZA", "CHEESE PIZZA"];
+var biryani_items = ["Chicken Biryani", "Mutton Biryani", "Veg Biryani", "Paneer Biryani"];
+var pizza_items = ["Chicken Pizza", "Veg Pizza", "Cheese Pizza"];
+var cart = "";
+(function() {
+  var item_categories = ["Biryani", "Pizza"];
+  var classNames = ["biryani", "pizza"];
+  var divWrap = document.createElement("div");
+  item_categories.forEach((item, index) => {
+    var textNode = document.createTextNode(item);
+    var divElement = document.createElement("div");
+    divElement.appendChild(textNode);
+    divElement.setAttribute("id", item);
+    divElement.classList.add(classNames[index], "menu-categories");
+    divWrap.appendChild(divElement);
+    divWrap.classList.add("flex-container");
+    document.getElementById("menu").append(divWrap);
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("menu-categories").style.display = "none";
+  });
 
-function displayMainMenu(){
-    document.getElementById("food").style.display="block";
-    document.getElementById("biryani").style.display="none";
-    document.getElementById("pizza").style.display="none";
-    document.getElementById("chooseBiryani").onclick=displayBiryaniMenu;
-    document.getElementById("choosePizza").onclick=displayPizzaMenu;
-}
+  document.getElementById("Biryani").onclick = displayBiryaniMenu;
+  document.getElementById("Pizza").onclick = displayPizzaMenu;
 
-function displayBiryaniMenu(){
-  document.getElementById("food").style.display="none";
-  document.getElementById("biryani").style.display="block";
-  //Todo: Innclude a let/const/var for all variables
-  str="";
-  for(var i=0; i<biryani_items.length; i++){
-    str+="<li id="+i+" onClick='addToCart_b(this.id)'>"+biryani_items[i]+"</li>";
+
+  function displayBiryaniMenu() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("menu-categories").style.display = "block";
+    document.getElementById("title").innerHTML = "Biryani";
+    let str = "";
+    for (let i = 0; i < biryani_items.length; i++) {
+      str += "<li id=" + i + " onClick='addToCart_biryani(this.id)'>" + biryani_items[i] + "</li>";
+    }
+    document.getElementById("menu_items_list").innerHTML = str;
+    document.getElementById("menu_items_list").classList.add("menu-items-list", "biryaniVarieties");
+    document.getElementById("back-btn").addEventListener("click", myFunc);
   }
-  document.getElementById("biryaniVarieties").innerHTML=str;
-  document.getElementById("back_b").onclick= displayMainMenu;
-}
 
-function displayPizzaMenu(){
-  document.getElementById("food").style.display="none";
-  document.getElementById("pizza").style.display="block";
-  str="";
-  for(var i=0; i<pizza_items.length; i++){
-    str+="<li id="+i+" onClick='this.addToCart_p(this.id)'>"+pizza_items[i]+"</li>";
+  function displayPizzaMenu() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("menu-categories").style.display = "block";
+    document.getElementById("title").innerHTML = "Pizza";
+
+    let str = "";
+    for (let i = 0; i < pizza_items.length; i++) {
+      str += "<li id=" + i + " onClick='addToCart_pizza(this.id)'>" + pizza_items[i] + "</li>";
+    }
+    document.getElementById("menu_items_list").innerHTML = str;
+    document.getElementById("menu_items_list").classList.add("menu-items-list", "pizzaVarieties");
+    document.getElementById("back-btn").addEventListener("click", myFunc);
   }
-  document.getElementById("pizzaVarieties").innerHTML=str;
-  document.getElementById("back_p").onclick= displayMainMenu;
-}
-  //Todo: Innclude all global variables at start of file
 
-var cart="";
-function addToCart_b(id_value){
-    cart+="<li>"+biryani_items[id_value]+"</li>";
-    document.getElementById("addToCart").innerHTML=cart;
-}
-// TODO:Function logic is repeated. Make it reusable
-function addToCart_p(id_value){
-    cart+="<li>"+pizza_items[id_value]+"</li>";
-    document.getElementById("addToCart").innerHTML=cart;
-}
-displayMainMenu();
+  function myFunc() {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("menu-categories").style.display = "none";
+  }
+
 })();
+
+function addToCart_biryani(value) {
+  cart += "<li>" + biryani_items[value] + "</li>";
+  document.getElementById("cart_items").innerHTML = cart;
+}
+
+function addToCart_pizza(value) {
+  cart += "<li>" + pizza_items[value] + "</li>";
+  document.getElementById("cart_items").innerHTML = cart;
+}
