eba6fe5062041101682686df3739f87291c71292
sowmikach <65588608+sowmikach@users.noreply.github.com>
2020-07-01 00:24:37 +0530

    Add files via upload

---
+++ Exercise14/script/info.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+var movies =[
+  {
+    "title":"Frozen",
+    "poster":"https://runt-of-the-web.com/wordpress/wp-content/uploads/2014/08/frozen.jpg",
+    "releaseDate":"2013-11-29",
+    "isRestricted":false
+  },
+  {
+    "title":"The Lion King",
+    "poster":"https://geekologie.com/2016/02/01/tim-burton-disney-movie-posters-10.jpg",
+    "releaseDate":"2019-07-09",
+    "isRestricted":false
+  },
+  {
+    "title":"Venom",
+    "poster":"https://upload.wikimedia.org/wikipedia/en/1/18/Venom_%282018_film_poster%29.png",
+    "releaseDate":"2018-02-18",
+    "isRestricted":true
+  },
+  {
+    "title":"The Adventures of Tin Tin",
+    "poster":"https://cdn3.movieweb.com/i/movie/qlfUJQVZTalq5TsDB2oa63o3VH75J2/1000:100/The-Adventures-Of-Tintin.jpg",
+    "releaseDate":"2016-12-18",
+    "isRestricted":false
+  },
+  {
+    "title":"Bahubali",
+    "poster":"https://images-na.ssl-images-amazon.com/images/I/711eHgGtnFL._SL1209_.jpg",
+    "releaseDate":"2018-04-28",
+    "isRestricted":false
+  },
+  {
+    "title":"Anabella",
+    "poster":"https://s3.r29static.com/bin/entry/466/x,80/1302167/annabelle-embed1.jpg",
+    "releaseDate":"2014-11-29",
+    "isRestricted":true
+  },
+  {
+    "title":"Jai Lava Kusa",
+    "poster":"https://gumlet.assettype.com/thequint/2017-09/74138995-ad5a-41fc-a73a-fe7bfa43a564/jai_lava_kusa_the_quint.jpg",
+    "releaseDate":"2012-09-21",
+    "isRestricted":false
+  },
+  {
+    "title":"Avatar",
+    "poster":"https://cdn1.thr.com/sites/default/files/2019/03/avatar-publicity_still-h_2019.jpg",
+    "releaseDate":"2009-12-18",
+    "isRestricted":true
+  },
+  {
+    "title":"Aruvi",
+    "poster":"https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/large/aruvi-et00064921-09-11-2017-01-25-36.jpg",
+    "releaseDate":"2010-07-09",
+    "isRestricted":true
+  },
+  {
+    "title":"Ala Viakuntapuramulo",
+    "poster":"https://m.media-amazon.com/images/M/MV5BYzVjNThjYzgtODRhYS00N2M0LTg5OWQtMTA0YjBhODJhNzU3XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    "releaseDate":"2020-01-12",
+    "isRestricted":false
+  }
+];
