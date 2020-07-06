645902f4284991d900d502cdf482c4fac2a77792
sowmikach <65588608+sowmikach@users.noreply.github.com>
2020-07-06 18:35:21 +0530

    Update script.js

--- Exercise13/script/script.js
+++ Exercise13/script/script.js
@@ -1,8 +1,9 @@
 //jshint esversion:6
-var biryani_items = ["Chicken Biryani", "Mutton Biryani", "Veg Biryani", "Paneer Biryani"];
-var pizza_items = ["Chicken Pizza", "Veg Pizza", "Cheese Pizza"];
-var cart = "";
+
 (function() {
+  var biryani_items = ["Chicken Biryani", "Mutton Biryani", "Veg Biryani", "Paneer Biryani"];
+  var pizza_items = ["Chicken Pizza", "Veg Pizza", "Cheese Pizza"];
+  var cart = "";
   var item_categories = ["Biryani", "Pizza"];
   var classNames = ["biryani", "pizza"];
   var divWrap = document.createElement("div");
@@ -18,69 +19,34 @@ var cart = "";
     document.getElementById("menu").style.display = "block";
     document.getElementById("menu-categories").style.display = "none";
   });
-//TODO: Make the code more generic. Example is provided
-  document.getElementById("Biryani").onclick = function(){displayMenu('Biriyani',biryani_items,"biryaniVarieties")};
-  document.getElementById("Pizza").onclick = function(){displayMenu('Pizza',pizza_items,"pizzaVarieties")};
 
-
-  function displayBiryaniMenu() {
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("menu-categories").style.display = "block";
-    document.getElementById("title").innerHTML = "Biryani";
-    let str = "";
-    for (let i = 0; i < biryani_items.length; i++) {
-      str += "<li id=" + i + " onClick='addToCart_biryani(this.id)'>" + biryani_items[i] + "</li>";
-    }
-    document.getElementById("menu_items_list").innerHTML = str;
-    document.getElementById("menu_items_list").classList.add("menu-items-list", "biryaniVarieties");
-    document.getElementById("back-btn").addEventListener("click", myFunc);
-  }
-
-  function displayPizzaMenu() {
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("menu-categories").style.display = "block";
-    document.getElementById("title").innerHTML = "Pizza";
-
-    let str = "";
-    for (let i = 0; i < pizza_items.length; i++) {
-      str += "<li id=" + i + " onClick='addToCart_pizza(this.id)'>" + pizza_items[i] + "</li>";
-    }
-    document.getElementById("menu_items_list").innerHTML = str;
-    document.getElementById("menu_items_list").classList.add("menu-items-list", "pizzaVarieties");
-    document.getElementById("back-btn").addEventListener("click", myFunc);
-  }
+  document.getElementById("Biryani").onclick = function(){displayMenu('Biriyani',biryani_items,"biryaniVarieties");};
+  document.getElementById("Pizza").onclick = function(){displayMenu('Pizza',pizza_items,"pizzaVarieties");};
 
   function displayMenu(title,items,className) {
     document.getElementById("menu").style.display = "none";
     document.getElementById("menu-categories").style.display = "block";
     document.getElementById("title").innerHTML = title;
     let str = "";
-    for (let i = 0; i < items.length; i++) {
-      str += "<li id=" + i + " onClick='addToCart_biryani(this.id)'>" + items[i] + "</li>";
+    for (let i = 0; i <items.length; i++) {
+      str += "<li id=" + i + " class='varieties'>" + items[i] + "</li>";
     }
     document.getElementById("menu_items_list").innerHTML = str;
-    document.getElementById("menu_items_list").classList.add("menu-items-list", className);
-    document.getElementById("back-btn").addEventListener("click", myFunc);
-  }
-  function myFunc() {
-    document.getElementById("menu").style.display = "block";
-    document.getElementById("menu-categories").style.display = "none";
+    document.getElementById("menu_items_list").classList.add("menu-items-list",className);
+
+    var userSelection = document.getElementsByClassName('varieties');
+    for(let i = 0; i < userSelection.length; i++) {
+      userSelection[i].addEventListener("click", function() {
+        cart += "<li>" + items[i] + "</li>";
+        document.getElementById("cart_items").innerHTML = cart;
+      });
+    }
+
+    document.getElementById("back-btn").addEventListener("click", function(){
+      document.getElementById("menu").style.display = "block";
+      document.getElementById("menu-categories").style.display = "none";
+      document.getElementById("menu_items_list").classList.remove(className);
+    });
   }
 
 })();
-
-function addToCart(value) {
-  cart += "<li>" + value + "</li>";
-  //Explore the append function
-  document.getElementById("cart_items").innerHTML = cart;
-}
-
-function addToCart_biryani(value) {
-  cart += "<li>" + biryani_items[value] + "</li>";
-  document.getElementById("cart_items").innerHTML = cart;
-}
-
-function addToCart_pizza(value) {
-  cart += "<li>" + pizza_items[value] + "</li>";
-  document.getElementById("cart_items").innerHTML = cart;
-}
