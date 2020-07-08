49d4d57ea5c5a27cf3327980ef1cbfccfaa52290
sowmikach <65588608+sowmikach@users.noreply.github.com>
2020-07-08 12:07:36 +0530

    Update script.js

--- Exercise17/script/script.js
+++ Exercise17/script/script.js
@@ -1,20 +1,24 @@
 //jshint esversion:6
-//Can we try this using AJAX call ?
-//mocky.io
-document ready (){
-  ajax call("get"){
-    sucesss(){
-      // call the function that builds the UI
-//pass the response from API->movies
-    }
-    error(){
-// Console the error
-    }
-  }
-}
 
-//named function
-function constructUI(){
+$(document).ready(function(){
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET','https://run.mocky.io/v3/a019288e-55b9-4731-86ef-0c9b9251e596');
+  ourRequest.onload= function(){
+    if(ourRequest.status >= 200 && ourRequest.status<400){
+      var movies = JSON.parse(ourRequest.responseText);
+      displayMovies(movies);
+    }else{
+      console.log("Connected to server but it returned an error.");
+    }
+  };
+  ourRequest.onerror = function(){
+    console.log("Connection error");
+  };
+  ourRequest.send();
+});
+
+// ---------------- Method : Display Movies ---------------------
+function displayMovies(movies){
   for(let i=0;i<movies.length;i++){
 
     let div=document.createElement("div");
@@ -64,5 +68,102 @@ function constructUI(){
     /* ------------------ End : Movie Certificate --------------*/
 
     document.getElementById("movies-wrapper").append(div);
+
   }
-};
+}
+
+// ---------------- Method : Display Popup ---------------------
+ function displayPopup(i){
+     document.getElementById("movies-wrapper").style.opacity="0.3";
+     document.body.style.overflow="hidden";
+
+     //-------------------- Popup Wrapper ---------------------
+     var popWrapper=document.createElement("div");
+     popWrapper.setAttribute("id","popWrapper");
+
+     //-------------------- Popup Content ---------------------
+     var popupDiv = document.createElement("div");
+     popupDiv.setAttribute("id","pop_description");
+     popupDiv.setAttribute("class","pop_description");
+
+     var pop = document.createElement("div");
+
+     //-------------------- Popup Image ---------------------
+     var popImgWrapper = document.createElement("div");
+     popImgWrapper.setAttribute("id","popImgWrapper");
+
+     var popImg = document.createElement("img");
+     popImg.setAttribute("id","popImg");
+     popImg.setAttribute("src",movies[i].poster);
+     if(movies[i].isRestricted===true){
+         var adultdiv=document.createElement("div");
+         adultdiv.setAttribute("class","adult");
+         var adiv=document.createElement("div");
+         adiv.setAttribute("class","adult-tag");
+         var adultTag=document.createTextNode("A");
+         adiv.append(adultTag);
+         adultdiv.append(adiv);
+         popImgWrapper.append(adultdiv);
+     }
+     popImgWrapper.append(popImg);
+     pop.append(popImgWrapper);
+
+     //-------------------- Popup Content ---------------------
+     var movieDescription = document.createElement("div");
+     movieDescription.setAttribute("id","movieDescription");
+
+     //-------------------- Popup Movie Title ---------------------
+     var titlewrap = document.createElement("div");
+     titlewrap.setAttribute("id","titlewrap");
+     var popTitle = document.createTextNode(movies[i].title);
+     titlewrap.append(popTitle);
+     movieDescription.append(titlewrap);
+
+     //-------------------- Popup Movie Language ---------------------
+     var langdiv= document.createElement("div");
+     langdiv.setAttribute("id","langdiv");
+     var langwrapper = document.createElement("div");
+     var lang = document.createTextNode(movies[i].language);
+     langwrapper.append(lang);
+     langdiv.append(langwrapper);
+     movieDescription.append(langdiv);
+
+     //-------------------- Popup Movie Description ---------------------
+     var desc=document.createTextNode(movies[i].description);
+     movieDescription.append(desc);
+
+     //-------------------- Popup Movie Likes ---------------------
+     var thumbsupDiv= document.createElement("div");
+     thumbsupDiv.setAttribute("id","thumbsupIconDiv");
+     var thumbsupIcon= document.createElement("i");
+     thumbsupIcon.setAttribute("class","fa fa-thumbs-up");
+     thumbsupDiv.append(thumbsupIcon);
+
+     var likesSpan= document.createElement("div");
+     likesSpan.setAttribute("id","likesSpan");
+     var like = document.createTextNode(movies[i].likes);
+     likesSpan.append(like);
+     thumbsupDiv.append(likesSpan);
+     movieDescription.append(thumbsupDiv);
+     pop.append(movieDescription);
+     popupDiv.append(pop);
+
+     //-------------------- Popup Close icon ---------------------
+     var closeicon= document.createElement("i");
+     closeicon.setAttribute("class","fa fa-times-circle");
+     closeicon.setAttribute("id","closeicon");
+     closeicon.setAttribute("onclick","hidePop()");
+     popupDiv.append(closeicon);
+     popWrapper.append(popupDiv);
+     document.body.append(popWrapper);
+ }
+
+
+  // ---------------- Method : hidePop ---------------------
+ function hidePop(){
+
+     var element = document.getElementById("popWrapper");
+     element.parentNode.removeChild(element);
+     document.getElementById("movies-wrapper").style.opacity="1";
+     document.body.style.overflow="visible";
+ }
