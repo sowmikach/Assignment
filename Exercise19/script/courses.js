0b12f01948c7e91d4e10c6546d4c05982fe54373
sowmikach <65588608+sowmikach@users.noreply.github.com>
2020-07-13 00:04:08 +0530

    Add files via upload

---
+++ Exercise19/script/courses.js
@@ -0,0 +1,32 @@
+var courses = [
+    {
+        "id" : 1,
+        "title" : "HTML 5 Training",
+        "video" : "https://www.w3schools.com/html/mov_bbb.mp4"
+    },
+    {
+        "id" : 2,
+        "title" : "CSS Training",
+        "video" : "https://www.w3schools.com/html/movie.mp4"
+    },
+    {
+        "id" : 3,
+        "title" : "Introduction to the world of React",
+        "video" : "https://tutorialehtml.com/assets_tutorials/media/Shaun-the-Sheep-The-Movie-Official-Trailer.mp4"
+    },
+    {
+        "id" : 4,
+        "title" : "Angular Crash Course",
+        "video" : "https://cdn.bitdegree.org/learn/Pexels%20Videos%203373.mp4?raw=true"
+    },
+    {
+        "id" : 5,
+        "title" : "What's cooking with jQuery?",
+        "video" : "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm"
+    },
+    {
+        "id" : 6,
+        "title" : "ES6 & Typescript",
+        "video" : "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.webm"
+    }
+];
---
+++ Exercise19/script/script.js
@@ -0,0 +1,99 @@
+// jshint esversion:6
+var x = document.getElementById("coordinates");
+var top = document.getElementById("goTop");
+var noOfReview = document.getElementById("noOfReview");
+var review_btn = document.getElementById("review-btn");
+var course = '';
+var comment = '';
+const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var REVIEWS_PER_PAGE = 3;
+
+$(document).ready(function() {
+  //ajax call
+  // $.ajax({
+  //   METHOD:'GET',
+  //   url:'jsonplaceholder.typicode.com/posts'
+  // }).done(function(courses){
+  //     var str='';
+  //     for(let i=0;i<courses.length;i++){
+  //       str +='<div class="video-list-item">'+
+  //               // '<video controls><source src='+courses[i].video+ 'type="video/mp4"></video>'+
+  //               '<h1>'+courses[i].title+'</h1>'+
+  //             '</div>';
+  //     }
+  //     $("#video-list-wrapper").append(str);
+  // });
+
+  //Navigation bar
+  $('#navigation li').on('click', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
+  //add reviews
+  review_btn.addEventListener("click", function() {
+    var d = new Date();
+    comment = document.getElementById("commentText").value;
+    if (comment != "") {
+      var reviewObj = {
+        user: "Sowmika",
+        profileImg: "https://i.pinimg.com/originals/78/2a/90/782a909dda83dc4fb1ee4855a5bdd317.png",
+        comment: comment,
+        likes: 0,
+        reviewDate: MONTHS[d.getMonth()] + " " + d.getDate()
+
+      };
+    }
+    document.getElementById("commentText").value = "";
+  });
+
+  //review list
+  var start = 0;
+  if (((start * REVIEWS_PER_PAGE) + REVIEWS_PER_PAGE) < REVIEWS.length)
+    len = ((start * REVIEWS_PER_PAGE) + REVIEWS_PER_PAGE);
+  else
+    len = REVIEWS.length;
+
+  for (let i = (start * REVIEWS_PER_PAGE); i < len; i++) {
+    comment += '<div class="review-list-item">' +
+      '<div class="profile left"><img src=' + REVIEWS[i].profileImg + ' alt="profile pic"></div>' +
+      '<div class="person-profile left">' +
+      '<h1 class="name">' + REVIEWS[i].user + '</h1>' +
+      '<p class="comment">' + REVIEWS[i].comment + '</p>' +
+      '<div class="thumbsup"><i class="fa fa-thumbs-up"></i>' +
+      '<span>' + REVIEWS[i].likes + '</span>' +
+      '<p class="right day">' + MONTHS[REVIEWS[i].timeStamp.slice(5, 7) - 1] + ' ' + REVIEWS[i].timeStamp.slice(8, 10) + '</p>' +
+      '</div>' +
+      '</div>' +
+      '<div class="clearfix"></div>' +
+      '</div>';
+  }
+  $("#review-list-wrapper").append(comment);
+  noOfReview.innerHTML = REVIEWS.length;
+
+  //video
+  for (let i = 0; i < courses.length; i++) {
+    course += '<div class="video-list-item">' +
+      '<video controls><source src=' + courses[i].video + ' type="video/mp4"></video>' +
+      '<h1>' + courses[i].title + '</h1>' +
+      '</div>';
+  }
+  $("#video-list-wrapper").append(course);
+
+  //------------------- Footer Section ---------------
+
+  //Location Coordinates
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $("#coordinates").html("  Latitude " + (position.coords.latitude).toFixed(2) +
+        ", Longitude " + (position.coords.longitude).toFixed(2));
+    });
+  } else {
+    console.log("Geolocation does not work in this browser");
+  }
+
+  // Go Back to Top of the page
+  top.addEventListener("click", function() {
+    $(window).scrollTop(0);
+  });
+
+});
