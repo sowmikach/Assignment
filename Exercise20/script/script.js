f1c338e3fe1f67cbf293f9fa92df7187b625f8c0
sowmikach <65588608+sowmikach@users.noreply.github.com>
2020-07-20 01:09:55 +0530

    Update script.js

--- Exercise20/script/script.js
+++ Exercise20/script/script.js
@@ -1,75 +1,104 @@
 //jshint esversion:6
-const target_date = "Jul 27 2020 12:00:00";
-var interval;
-var x = 0;
-// Styling of layout
-// Working of carousel
-//Add a input for event start date and button to trigger the timer
-//Font - Roboto 
 
 $(document).ready(function() {
 
+  // --------------------- Carousel ----------------------
+  var slideIndex = 1;
+  var myTimer;
+
   $(".fa-chevron-circle-left").click(function() {
-    if (x < 0)
-      x += 762;
-    clearInterval(interval);
-    interval = setInterval(slideImage, 3000);
-    $(".slide").css("left", x + "px");
+    plusSlides(-1);
   });
 
   $(".fa-chevron-circle-right").click(function() {
-    if (x >= -1600)
-      x -= 762;
-    clearInterval(interval);
-    interval = setInterval(slideImage, 3000);
-    $(".slide").css("left", x + "px");
+    plusSlides(1);
   });
 
   /**
-  * @function slideImage
-  */
-  function slideImage() {
-    if (x < -1600) {
-      x = 0;
-      $(".slide").css("left", x + "px");
-      $(".slide").css("transition", "0s");
+   * @function plusSlides
+   * @param {number} n - move next or previous
+   */
+  function plusSlides(n) {
+    clearInterval(myTimer);
+    if (n < 0) {
+      showSlides(slideIndex -= 1);
     } else {
-      x -= 762;
-      $(".slide").css("left", x + "px");
-      $(".slide").css("transition", "1s ease-in-out");
+      showSlides(slideIndex += 1);
+    }
+    if (n === -1) {
+      myTimer = setInterval(function() {
+        plusSlides(n + 2);
+      }, 3000);
+    } else {
+      myTimer = setInterval(function() {
+        plusSlides(n);
+      }, 3000);
     }
   }
-  interval = setInterval(slideImage, 3000);
 
-  var countDownDate = new Date(target_date).getTime();
-  setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    let days = format(Math.floor(distance / (1000 * 60 * 60 * 24)));
-    let hours = format(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    let minutes = format(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-    let seconds = format(Math.floor((distance % (1000 * 60)) / 1000));
-
-    /**
-    * @function format
-    * @param {number} singleDigit
-    */
-    function format(singleDigit) {
-      if (singleDigit < 10)
-        singleDigit = "0" + singleDigit;
-      return singleDigit;
+  /**
+   * @function showSlides
+   * @param {number} n - slide number
+   */
+  function showSlides(n) {
+    var i;
+    var slides = $(".slide");
+    if (n > slides.length) {
+      slideIndex = 1;
     }
-
-    $("#timer").html('<span class="days">' + days + '<span>D</span></span>' +
-      '<span class="hours">' + hours + '<span>H</span></span>' +
-      '<span class="minutes">' + minutes + '<span>M</span></span>' +
-      '<span class="seconds">' + seconds + '<span>S</span></span>');
-      
-    if (distance < 0) {
-      clearInterval(x);
-      $("#timer").html("EXPIRED");
+    if (n < 1) {
+      slideIndex = slides.length;
     }
-  }, 1000);
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
+  showSlides(slideIndex);
+  myTimer = setInterval(function() {
+    plusSlides(1);
+  }, 3000);
 
+  // ------------------- Count Down Timer ----------------
+  $("#timer").html("Count Down");
+
+  $("#datepicker").datepicker({
+    minDate: 1
+  });
+
+  $("#startTimer").click(function() {
+    $(".formWrapper").hide();
+    var countDownDate = new Date($("#datepicker").val()).getTime();
+    setInterval(function() {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      let days = format(Math.floor(distance / (1000 * 60 * 60 * 24)));
+      let hours = format(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      let minutes = format(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+      let seconds = format(Math.floor((distance % (1000 * 60)) / 1000));
+
+      /**
+       * @function format
+       * @param {number} singleDigit
+       */
+      function format(singleDigit) {
+        if (singleDigit < 10)
+          singleDigit = "0" + singleDigit;
+        return singleDigit;
+      }
+
+      $("#timer").html('<span class="days">' + days + '<span>D</span></span>' +
+        '<span class="hours">' + hours + '<span>H</span></span>' +
+        '<span class="minutes">' + minutes + '<span>M</span></span>' +
+        '<span class="seconds">' + seconds + '<span>S</span></span>');
+
+      if (distance < 0) {
+        clearInterval(x);
+        $("#timer").html("EXPIRED");
+      }
+    }, 1000);
+  });
+
+// --------------------- Accordion --------------------
   $("#accordion").accordion();
 });
